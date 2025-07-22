fa0ebda0c45b2203e170ff2fed10d8d168c0f774
Greg <grzegorz@lowgular.io>
2025-07-22 08:00:15 +0100

    updated to 20

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -10,52 +10,53 @@ import { ProductModel } from './products.service';
     <div class="product-detail-container rounded">
       <div class="product-detail-card">
         @if (product(); as product) {
-        <div
-          class="product-image-wrapper"
-          [ngStyle]="{ 'background-color': selectedColor() }"
-        >
-          <img
-            [src]="product.image"
-            [alt]="product.name"
-            class="product-image"
-          />
-        </div>
-        <div class="product-info">
-          <h2 class="product-title">{{ product.name }}</h2>
-          <p class="product-price text-primary">
-            {{ product.price | currency }}
-          </p>
-          <p class="product-description">
-            {{ product.description }}
-          </p>
-          <div class="color-picker">
-            <span
-              *ngFor="let color of colors"
-              class="color-circle"
-              [ngStyle]="{ 'background-color': color }"
-              [class.selected]="color === selectedColor()"
-              (click)="selectColor(color)"
-            ></span>
+          <div
+            class="product-image-wrapper"
+            [ngStyle]="{ 'background-color': selectedColor() }"
+            >
+            <img
+              [src]="product.image"
+              [alt]="product.name"
+              class="product-image"
+              />
           </div>
-        </div>
+          <div class="product-info">
+            <h2 class="product-title">{{ product.name }}</h2>
+            <p class="product-price text-primary">
+              {{ product.price | currency }}
+            </p>
+            <p class="product-description">
+              {{ product.description }}
+            </p>
+            <div class="color-picker">
+              @for (color of colors; track color) {
+                <span
+                  class="color-circle"
+                  [ngStyle]="{ 'background-color': color }"
+                  [class.selected]="color === selectedColor()"
+                  (click)="selectColor(color)"
+                ></span>
+              }
+            </div>
+          </div>
         } @else {
-        <div class="product-image-wrapper">
-          <img
-            src="https://dummyimage.com/150x150/cccccc/888888&text=N/A"
-            alt="No product selected"
-            class="product-image"
-          />
-        </div>
-        <div class="product-info">
-          <h2 class="product-title">No product selected</h2>
-          <p class="product-description">
-            Choose a product from the list to view its details
-          </p>
-        </div>
+          <div class="product-image-wrapper">
+            <img
+              src="https://dummyimage.com/150x150/cccccc/888888&text=N/A"
+              alt="No product selected"
+              class="product-image"
+              />
+          </div>
+          <div class="product-info">
+            <h2 class="product-title">No product selected</h2>
+            <p class="product-description">
+              Choose a product from the list to view its details
+            </p>
+          </div>
         }
       </div>
     </div>
-  `
+    `
 })
 export class ProductDetailComponent {
   product = signal<ProductModel | undefined>(undefined);
